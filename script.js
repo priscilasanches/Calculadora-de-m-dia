8ed8d7e0d28b51c733a9526913dfec940f978a88
Priscila <psjanuario@gmail.com>
2021-11-13 20:11:02 -0300

    Correção JS

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function calcularMedia(){
   let primeiraNota = parseFloat(document.getElementById("primeiranota").value);
-  let segundaNota = parseFloat(document.getElementById("nota2").value);
-  let terceiraNota = parseFloat(document.getElementById("nota3").value);
-  let quartaNota = parseFloat(document.getElementById("nota4").value);
+  let segundaNota = parseFloat(document.getElementById("segundanota").value);
+  let terceiraNota = parseFloat(document.getElementById("terceiranota").value);
+  let quartaNota = parseFloat(document.getElementById("quartanota").value);
 
   let somaNotas = (primeiraNota + segundaNota + terceiraNota + quartaNota);
  return somaNotas/4;
 }  
 
 function mostraResultado(){
-  //var notaFinal = calcularMedia();
+  var notaFinal = calcularMedia();
   var resultado;
   var textoResultado = document.getElementById("resultado");
   
-  //if (notaFinal>=7){
-  //  resultado = "Parabéns, sua média final foi " + notaFinal.toFixed(2) + "! Você foi aprovado.";
-  //} else if (notaFinal < 7 && notaFinal>=5){
-   // resultado = "Sua média final foi " + notaFinal.toFixed(2) + ", não sendo suficiente para a aprovação.<br> Realize o exame final para complementar sua nota."
-  //} else {
-   // resultado = "Sua média final foi " + notaFinal.toFixed(2) + ". Você foi reprovado."
-  //}
+  if (notaFinal>=7){
+    resultado = "Parabéns, sua média final foi " + notaFinal.toFixed(2) + "!<br> Você foi aprovado.";
+  } else if (notaFinal < 7 && notaFinal>=5){
+    resultado = "Sua média final foi " + notaFinal.toFixed(2) + ", não sendo suficiente para a aprovação.<br> Realize o exame final para complementar sua nota."
+  } else if (notaFinal < 5){
+    resultado = "Sua média final foi " + notaFinal.toFixed(2) + ".<br> Você foi reprovado."
+  } else
+    resultado = "Por favor, informe a nota das quatro provas."
   
-  textoResultado.innerHTML = "estude mais";
+  textoResultado.innerHTML = resultado;
   
 }
 
